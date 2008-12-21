c7562d470a442d3d2762f7a0d114fc80ed347068
Jeremy Hinegardner <jeremy@hinegardner.org>
2008-12-20 22:31:29 -0700

    update docs for mutexed stats

--- lib/hitimes/mutexed_stats.rb
+++ lib/hitimes/mutexed_stats.rb
@@ -2,6 +2,11 @@ require 'hitimes'
 require 'thread'
 
 module Hitimes
+  #
+  # MutexedStats is the start of a threadsafe Stats class.  Currently, on MRI
+  # Ruby the Stats object is already threadsafe, so there is no need to use
+  # MutexedStats.
+  #
   class MutexedStats < Stats
     def initialize
       @mutex = Mutex.new
