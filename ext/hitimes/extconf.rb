0ebfee4f0968d7779d37028366a294e61b4fec91
Benoit Daloze <eregontp@gmail.com>
2012-08-29 12:24:31 +0200

    use RbConfig instead of deprecated Config

--- ext/hitimes/extconf.rb
+++ ext/hitimes/extconf.rb
@@ -1,10 +1,10 @@
 require 'rbconfig'
 require 'mkmf'
 
-if Config::CONFIG['host_os'] =~ /darwin/ then
+if RbConfig::CONFIG['host_os'] =~ /darwin/ then
   $CFLAGS += " -DUSE_INSTANT_OSX=1 -Wall"
   $LDFLAGS += " -framework CoreServices"
-elsif Config::CONFIG['host_os'] =~ /win32/ or Config::CONFIG['host_os'] =~ /mingw/ then
+elsif RbConfig::CONFIG['host_os'] =~ /win32/ or RbConfig::CONFIG['host_os'] =~ /mingw/ then
   $CFLAGS += " -DUSE_INSTANT_WINDOWS=1"
 else
   if have_library("rt", "clock_gettime") then
