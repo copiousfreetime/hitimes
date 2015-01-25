3d8357dd2a4d9dfad3f527ad8a7a2530721bb974
Joel Low <joel@joelsplace.sg>
2015-01-25 20:27:33 +0800

    Add support for 64-bit Windows rubies.

--- ext/hitimes/c/extconf.rb
+++ ext/hitimes/c/extconf.rb
@@ -4,7 +4,7 @@ require 'mkmf'
 if RbConfig::CONFIG['host_os'] =~ /darwin/ then
   $CFLAGS += " -DUSE_INSTANT_OSX=1 -Wall"
   $LDFLAGS += " -framework CoreServices"
-elsif RbConfig::CONFIG['host_os'] =~ /win32/ or RbConfig::CONFIG['host_os'] =~ /mingw/ then
+elsif RbConfig::CONFIG['host_os'] =~ /win(32|64)/ or RbConfig::CONFIG['host_os'] =~ /mingw/ then
   $CFLAGS += " -DUSE_INSTANT_WINDOWS=1"
 else
   if have_library("rt", "clock_gettime") then
