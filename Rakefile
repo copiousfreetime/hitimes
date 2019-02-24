020dfb35a4c2ea5d4e272002713087a46377f0d0
copiousfreetime <jeremy@copiousfreetime.org>
2019-02-23 18:41:09 -0700

    remove the c and java extensions

--- Rakefile
+++ Rakefile
@@ -11,18 +11,9 @@ This.ruby_gemspec do |spec|
   spec.add_development_dependency( 'minitest'     , '~> 5.5' )
   spec.add_development_dependency( 'rdoc'         , '~> 6.0' )
   spec.add_development_dependency( 'json'         , '~> 2.2' )
-  spec.add_development_dependency( 'rake-compiler', '~> 1.0' )
-  spec.add_development_dependency( 'rake-compiler-dock', '~> 0.6' )
   spec.add_development_dependency( 'simplecov'    , '~> 0.14' )
 
-  spec.extensions.concat This.extension_conf_files
   spec.license = "ISC"
 end
 
-This.java_gemspec( This.ruby_gemspec ) do |spec|
-  spec.extensions.clear
-  spec.files << "lib/hitimes/hitimes.jar"
-end
-
 load 'tasks/default.rake'
-load 'tasks/extension.rake'
--- lib/hitimes.rb
+++ lib/hitimes.rb
@@ -26,36 +26,6 @@ end
 require 'hitimes/paths'
 require 'hitimes/version'
 require 'hitimes/instant'
-
-# Load the binary extension, try loading one for the specific version of ruby
-# and if that fails, then fall back to one in the top of the library.
-# this is the method recommended by rake-compiler
-
-attempts = [
-  "hitimes/#{RUBY_VERSION.sub(/\.\d+$/,'')}/hitimes",
-  "hitimes/hitimes"
-]
-loaded = false
-
-path_exceptions = []
-attempts.each do |path|
-  begin
-    require path
-    loaded = true
-    break
-  rescue LoadError => load_error
-    full_path = File.expand_path(path)
-    path_exceptions << [ full_path, load_error.message ]
-  end
-end
-
-if !loaded then
-  msg = ["Unable to find binary extension, was hitimes installed correctly? The following paths were tried."]
-  path_exceptions.each do |path, message|
-    msg << "#{path} : #{message}"
-  end
-  raise LoadError, msg.join("\n")
-end
 require 'hitimes/interval'
 
 require 'hitimes/stats'
