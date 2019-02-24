46e244a32bef7db06dc4abd8870e00f3e4f3e831
copiousfreetime <jeremy@copiousfreetime.org>
2019-02-24 11:43:42 -0700

    put resolution and clock into clock_description

--- lib/hitimes/instant.rb
+++ lib/hitimes/instant.rb
@@ -5,16 +5,18 @@ module Hitimes
   CLOCK_ID                        = Initialize.determine_clock_id.freeze
 
   # Public: The resolution of the clock
-  CLOCK_RESOLUTION_IN_NANOSECONDS = Process.clock_getres(CLOCK_ID, :nanosecond).freeze
+  CLOCK_RESOLUTION_NANOSECONDS    = Process.clock_getres(CLOCK_ID, :nanosecond).freeze
 
   # Internal: The fraction of second of a nanosecond
-  NANOSECONDS_PER_SECOND                 = 1e9
+  NANOSECONDS_PER_SECOND          = 1e9
 
-  # Public: The same as CLOCK_RESOLUTION here for API compatibility with
-  # previous versions of Hitimes.
+  # Public: The smallest fraction of a second hitimes can do
+  CLOCK_RESOLUTION_SECONDS        = CLOCK_RESOLUTION_NANOSECONDS / NANOSECONDS_PER_SECOND
+
+  # Public: The factor used to convert the instant values to fractional seconds
   #
   # The raw instant values are divided by this value to get float seconds
-  INSTANT_CONVERSION_FACTOR  = CLOCK_RESOLUTION_IN_NANOSECONDS * NANOSECONDS_PER_SECOND
+  INSTANT_CONVERSION_FACTOR       = CLOCK_RESOLUTION_NANOSECONDS * NANOSECONDS_PER_SECOND
 
 
   # Public: Get the raw instant
@@ -29,10 +31,10 @@ module Hitimes
   end
   module_function :raw_instant
 
-  # Internal: The human readable clock description of the CLOCK_ID as a string
+  # Internal: The human readable clock name of the CLOCK_ID as a string
   #
-  # Returns the clock description as a String
-  def clock_description
+  # Returns the clock name as a String
+  def clock_name
     case CLOCK_ID
     when Symbol
       CLOCK_ID.to_s
@@ -42,6 +44,22 @@ module Hitimes
       }
       "Process::#{const.to_s}"
     end
+  end
+  module_function :clock_name
+
+  # Internal: The human readable clock resolution
+  #
+  # Returns the clock resolution as a string
+  def clock_resolution_description
+    "#{CLOCK_RESOLUTION_NANOSECONDS}ns"
+  end
+  module_function :clock_resolution_description
+
+  # Internal: The human readable clock description that is used by hitimes
+  #
+  # Returns the clock description as a String
+  def clock_description
+    "#{clock_name} #{clock_resolution_description}"
   end
   module_function :clock_description
 end
