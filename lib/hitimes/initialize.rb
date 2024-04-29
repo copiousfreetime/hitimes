9070bf02eb1e540058e269eab46b6e186d364d11
Jeremy Hinegardner <jeremy@copiousfreetime.org>
2024-04-28 23:59:42 -0600

    determine which clock to use

--- lib/hitimes/initialize.rb
+++ lib/hitimes/initialize.rb
@@ -4,82 +4,98 @@ module Hitimes
   # Internal: Internal setup that is done when the library is loaded
   #
   # We want to determine what clock to use for this machine. So we need to
-  # intropect the ruby environment and then setup some initial constants. These
+  # introspect the ruby environment and then setup some initial constants. These
   # methods are used in lib/hitimes/instant.rb to help setup the CLOCK_ID
-  # constant
+  # constant at load time.
   #
   module Initialize
-    # Internal: Return the list of clock ids, that we are going to test. We'll
-    # test to see if it exists, and then do further tests after that.
     #
-    # The one requirement is that they are monotonically increasing clocks
+    # After a fair bit of experimentaiton, it seems that the only clock_ids that
+    # are of any use are the following:
     #
-    # Returns an array of clock ids
-    def potential_clock_ids
-      [].tap do |clock_ids|
-        # General monotonic constants in general order of priority. We'll use
-        # the first one that matches.
-        #
-        %i[CLOCK_MONOTONIC_RAW CLOCK_BOOTTIME CLOCK_MONOTONIC].each do |c|
-          clock_ids << Process.const_get(c) if Process.const_defined?(c)
-        end
+    # POSIX:
+    #
+    #   CLOCK_REALTIME   A settable system-wide real-time clock. Measures
+    #                    wall-clock time. Affected by system jumps in time,
+    #                    adjtime(3), and NTP.
+    #
+    #   CLOCK_MONOTONIC  A nonsettable system-wide clock that represent
+    #                    monotomic time since some unspecified point in the
+    #                    past. Not affected by jumps in system time, but is
+    #                    affectd by adjtime(3) and NTP.
+    #
+    # Darwin:
+    #
+    #   CLOCK_MONOTONIC_RAW  clock that increments monotonically, tracking the
+    #                        time since an arbitrary point like CLOCK_MONOTONIC.
+    #                        However, this clock is unaffected by frequency or
+    #                        time adjustments.
+    #
+    #   CLOCK_UPTIME_RAW  clock that increments monotonically, in the same manner
+    #                     as CLOCK_MONOTONIC_RAW, but that does not increment
+    #                     while the system is asleep.  The returned value is
+    #                     identical to the result of mach_absolute_time()
+    #                     after the appropriate mach_timebase conversion is applied.
+    #
+    # Linux:
+    #
+    #   CLOCK_MONOTONIC_RAW  Similar to CLOCK_MONOTONIC, but provides access to
+    #                        a raw hardware-based time that is not subject to NTP
+    #                        adjustments or the incremental adjustments performed
+    #                        by adjtime(3)
+    #
+    #   CLOCK_BOOTTIME  Identical to CLOCK_MONOTONIC, except it also includes any
+    #                   time that the system is suspended.
+    #
+    # *BSD:
+    #
+    #   All the BSDs seem to have CLOCK_MONOTONIC and CLOCK_REALTIME although on
+    #   NetBSD CLOCK_MONOTONIC is not affected by adjtime(2). It is unclear if 
+    #   they are affected by adjtime(2) on FreeBSD, OpenBSD, or DragonFlyBSD. -
+    #   at least according to the man pages.
+    #
+    # What this boils down to as that pretty much all systems have CLOCK_REALTIME
+    # and CLOCK_MONOTONIC. The other clocks are system specific and may or may
+    # not exist. We'll try to use the most accurate clock available.
+    #
+    # So we'll try to use the following clocks in order of preference:
+    #
+    # On Linux and Darwin
+    #   CLOCK_MONOTONIC_RAW, CLOCK_MONOTONIC, CLOCK_REALTIME
+    #
+    # Everyone else:
+    #   CLOCK_MONOTONIC, CLOCK_REALTIME
+    #
+    # So in reality, well just test for constants on all of the above and use the
+    # first one that exists.
+    #
+    # If CLOCK_REALTIME is chose, we will dump a warning to the user.
+    # And if we can't finde one, which is really, really odd, we'll raise an exception.
+    POTENTIAL_CLOCK_IDS = %i[ CLOCK_MONOTONIC_RAW CLOCK_MONOTONIC CLOCK_REALTIME].freeze
+    def determine_clock_id(potential_ids = POTENTIAL_CLOCK_IDS)
+      sym = potential_ids.find { |c| Process.const_defined?(c) }
+
+      if :CLOCK_REALTIME == sym
+        warn <<~TXT
+        Unable to find a high resolution clock. Using CLOCK_REALTIME for timing.
+
+        RUBY_DESCRIPTION: #{RUBY_DESCRIPTION}
+
+        Please report the above information hitimes issue tracker at
+        https://github.com/copiousfreetime/hitimes/issuest
+        TXT
+      elsif sym.nil?
+        raise Hitimes::Error, <<~ERROR
+        Unable to find a high resolution clock at all. THIS IS A BUG!!
+
+        RUBY_DESCRIPTION: #{RUBY_DESCRIPTION}
+
+        Please report this bug to the hitimes issue tracker at
+        https://github.com/copiousfreetime/hitimes/issues
+        ERROR
       end
-    end
-    module_function :potential_clock_ids
 
-    # Internal: Determine the resolution of a single clock_id
-    #
-    # Since clock_getres is not reliable, do this in a different way. Since we
-    # can always make `clock_gettime` return in nanosecond resolution, it will
-    # have trailing zeros. We're going to pick the number of trailing zeros of
-    # the number and use that to guess its resolution.
-    #
-    # Randomly picked 17 times to call the clock_gettime.
-    #
-    # * https://bugs.ruby-lang.org/issues/16740
-    #
-    def resolution_of(clock_id)
-      counts = Hash.new(0)
-      17.times do
-        val = Process.clock_gettime(clock_id, :nanosecond)
-        res = if (val % 1_000_000_000).zero?
-                1
-              elsif (val % 1_000_000).zero?
-                1e-3
-              elsif (val % 1_000).zero?
-                1e-6
-              else
-                1e-9
-              end
-        counts[res] += 1
-      end
-
-      # now just resolution of the one that has the most values, which will be
-      # the last one when sorted.
-      ordered = counts.to_a.sort_by { |pair| pair[1] }
-      ordered.last[0]
-    end
-    module_function :resolution_of
-
-    # Internal: Determine what clock to use for the machine we are one. We want
-    # the highest resolution clock possible, which should be nanosecond
-    # resolution.
-    #
-    # Get the resolution of each clock id and then return the higest resolution
-    # id from the list.
-    #
-    # Returns the clock id to use on this ruby
-    def determine_clock_id
-      ids_and_resolutions = potential_clock_ids.map do |clock_id|
-        [clock_id, resolution_of(clock_id)]
-      end
-
-      # Sort them by the resolution - we want the smallest one first
-      ids_and_resolutions.sort_by! { |pair| pair[1] }
-      best_clock_and_resolution = ids_and_resolutions[0]
-
-      # return the clock id
-      best_clock_and_resolution[0]
+      Process.const_get(sym)
     end
     module_function :determine_clock_id
   end
